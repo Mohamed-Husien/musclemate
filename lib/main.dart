df6f6dfe4a8b3368a9e26f18149f791c088f5f20
MahmoudSherif28 <116498523+MahmoudSherif28@users.noreply.github.com>
2024-09-16 23:38:19 +0300

    edited splash.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart'; // Import screenutil
 import 'package:musclemate/helpers/color_extension.dart';
-import 'package:musclemate/views/menu/menu_view.dart';
 import 'package:musclemate/views/onboard/splash.dart';
 
 void main() {
@@ -9,17 +9,25 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Workout Fitness',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: "Quicksand",
-        colorScheme: ColorScheme.fromSeed(seedColor: TColor.primary),
-        useMaterial3: false,
-      ),
-      home: const splash(),
+    return ScreenUtilInit( // Initialize ScreenUtil here
+      designSize: const Size(375, 812), // Base design size (e.g., iPhone X)
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          title: 'Workout Fitness',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            fontFamily: "Quicksand",
+            colorScheme: ColorScheme.fromSeed(seedColor: TColor.primary),
+            useMaterial3: false,
+          ),
+          home: const splash(),
+        );
+      },
     );
   }
 }
